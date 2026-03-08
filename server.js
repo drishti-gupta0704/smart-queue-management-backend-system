e3ed8ac732a0a1a4d4fb72ca6cb1723eba1467fe
drishti-gupta0704 <drishti.gupta070424@gmail.com>
2026-03-08 13:32:17 +0530

    solving bug...changed the order of dotenv.config() and connectDB() .  dotenv.config() should be called before connectDB()

--- server.js
+++ server.js
@@ -1,20 +1,19 @@
-
 const express = require("express");
 const dotenv = require("dotenv");
 const cors = require("cors");
 const connectDB = require("./config/db");
 const authRoutes = require("./routes/authRoutes");
 
-connectDB();
+dotenv.config();     
 
-dotenv.config();
+connectDB();         
 
 const app = express();
 
-app.use("/api/auth", authRoutes);
-
 app.use(express.json());
 app.use(cors());
+
+app.use("/api/auth", authRoutes);
 
 app.listen(process.env.PORT, () => {
  console.log("Server running");
