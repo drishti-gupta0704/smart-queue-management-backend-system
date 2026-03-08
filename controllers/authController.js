b4dd8edc1a6f363e37676789da103bb04c224c47
drishti-gupta0704 <drishti.gupta070424@gmail.com>
2026-03-08 13:02:41 +0530

    adding auth controller logic to register user and login user

--- controllers/authController.js
+++ controllers/authController.js
@@ -1,3 +1,89 @@
 
 // registerUser
 // loginUser
+
+const User = require("../models/User");
+const bcrypt = require("bcryptjs");
+const generateToken = require("../utils/jwt");
+
+// REGISTER USER
+const registerUser = async (req, res) => {
+  try {
+    const { name, email, password } = req.body;
+
+    // checking if user already exists
+    const userExists = await User.findOne({ email });
+
+    if (userExists) {
+      return res.status(400).json({ message: "User already exists" });
+    }
+
+    // hash password
+    const hashedPassword = await bcrypt.hash(password, 10);
+
+    // create user
+    const user = await User.create({
+      name,
+      email,
+      password: hashedPassword,
+    });
+
+    res.status(201).json({
+      message: "User registered successfully",
+      user: {
+        id: user._id,
+        name: user.name,
+        email: user.email,
+        role: user.role
+      }
+    });
+
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
+};
+
+
+// LOGIN USER
+const loginUser = async (req, res) => {
+  try {
+    const { email, password } = req.body;
+
+    // find user
+    const user = await User.findOne({ email });
+
+    if (!user) {
+      return res.status(400).json({ message: "Invalid credentials" });
+    }
+
+    // compare password
+    const isMatch = await bcrypt.compare(password, user.password);
+
+    if (!isMatch) {
+      return res.status(400).json({ message: "Invalid credentials" });
+    }
+
+    // generate token
+    const token = generateToken(user._id);
+
+    res.status(200).json({
+      message: "Login successful",
+      token,
+      user: {
+        id: user._id,
+        name: user.name,
+        email: user.email,
+        role: user.role
+      }
+    });
+
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
+};
+
+
+module.exports = {
+  registerUser,
+  loginUser
+};
--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -3,7 +3,7 @@ const express = require("express");
 const router = express.Router();
 const { registerUser, loginUser } = require("../controllers/authController");
 
-router.post("/register", registerUser);
-router.post("/login", loginUser);
+router.post("/register", registerUser); // register user
+router.post("/login", loginUser);   // login user
 
 module.exports = router;
