e8510f4ca64f237bce09e8a618de2d8d052d5c53
drishti-gupta0704 <drishti.gupta070424@gmail.com>
2026-03-08 12:56:20 +0530

    adding auth routes logic and adding routes in server.js file

--- server.js
+++ server.js
@@ -3,11 +3,15 @@ const express = require("express");
 const dotenv = require("dotenv");
 const cors = require("cors");
 const connectDB = require("./config/db");
+const authRoutes = require("./routes/authRoutes");
+
 connectDB();
 
 dotenv.config();
 
 const app = express();
+
+app.use("/api/auth", authRoutes);
 
 app.use(express.json());
 app.use(cors());
