e7055c92d0420ba6c73c3609cb723a7812f03fd9
drishti-gupta0704 <drishti.gupta070424@gmail.com>
2026-03-09 20:23:56 +0530

    creating ticket model that represents user in a queue

---
+++ models/Ticket.js
@@ -0,0 +1,42 @@
+
+const mongoose = require("mongoose");
+
+const ticketSchema = new mongoose.Schema(
+  {
+    user: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "User",
+      required: true
+    },
+
+    queue: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "Queue",
+      required: true
+    },
+
+    tokenNumber: {
+      type: Number,
+      required: true
+    },
+
+    status: {
+      type: String,
+      enum: ["waiting", "serving", "completed", "cancelled"],
+      default: "waiting"
+    },
+
+    priority: {
+      type: Boolean,
+      default: false
+    },
+
+    joinedAt: {
+      type: Date,
+      default: Date.now
+    }
+  },
+  { timestamps: true }
+);
+
+module.exports = mongoose.model("Ticket", ticketSchema);
