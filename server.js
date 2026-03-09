603b88310bb38f5e764ec507d25abf48d7eb7627
drishti-gupta0704 <drishti.gupta070424@gmail.com>
2026-03-09 20:29:06 +0530

    updating server.js file

--- server.js
+++ server.js
@@ -2,13 +2,15 @@
 const express = require("express");
 const dotenv = require("dotenv");
 const cors = require("cors");
+
 const connectDB = require("./config/db");
 
 const authRoutes = require("./routes/authRoutes");
 const userRoutes = require("./routes/userRoutes");
+const queueRoutes = require("./routes/queueRoutes");
+const ticketRoutes = require("./routes/ticketRoutes");
 
 dotenv.config();
-
 connectDB();
 
 const app = express();
@@ -18,6 +20,8 @@ app.use(cors());
 
 app.use("/api/auth", authRoutes);
 app.use("/api/users", userRoutes);
+app.use("/api/queues", queueRoutes);
+app.use("/api/tickets", ticketRoutes);
 
 app.listen(process.env.PORT, () => {
   console.log(`Server running on port ${process.env.PORT}`);
