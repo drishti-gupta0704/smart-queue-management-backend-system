0918bed9e2a1dba6df7e8a7f6380bc98ee3a329e
drishti-gupta0704 <drishti.gupta070424@gmail.com>
2026-03-09 19:20:27 +0530

    updating server.js

---
+++ server.js
@@ -0,0 +1,24 @@
+
+const express = require("express");
+const dotenv = require("dotenv");
+const cors = require("cors");
+const connectDB = require("./config/db");
+
+const authRoutes = require("./routes/authRoutes");
+const userRoutes = require("./routes/userRoutes");
+
+dotenv.config();
+
+connectDB();
+
+const app = express();
+
+app.use(express.json());
+app.use(cors());
+
+app.use("/api/auth", authRoutes);
+app.use("/api/users", userRoutes);
+
+app.listen(process.env.PORT, () => {
+  console.log(`Server running on port ${process.env.PORT}`);
+});
