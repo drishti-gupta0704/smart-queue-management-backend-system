f2cd6ce734e0ef49eb03bd1c78df6b3afa98290f
drishti-gupta0704 <drishti.gupta070424@gmail.com>
2026-03-10 19:22:55 +0530

    adding avg serviceTime for wait time calculation.

--- models/Queue.js
+++ models/Queue.js
@@ -1,34 +1,32 @@
 
 const mongoose = require("mongoose");
 
-const queueSchema = new mongoose.Schema(
-  {
-    name: {
-      type: String,
-      required: true
-    },
-
-    location: {
-      type: String,
-      required: true
-    },
-
-    maxSize: {
-      type: Number,
-      default: 50
-    },
-
-    currentToken: {
-      type: Number,
-      default: 0
-    },
-
-    createdBy: {
-      type: mongoose.Schema.Types.ObjectId,
-      ref: "User"
-    }
+const queueSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: true
   },
-  { timestamps: true }
-);
+
+  location: {
+    type: String,
+    required: true
+  },
+
+  maxSize: {
+    type: Number,
+    default: 50
+  },
+
+  currentToken: {
+    type: Number,
+    default: 0
+  },
+
+  avgServiceTime: {
+    type: Number,
+    default: 5
+  }
+
+}, { timestamps: true });
 
 module.exports = mongoose.model("Queue", queueSchema);
