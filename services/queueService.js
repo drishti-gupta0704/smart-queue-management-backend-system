ea925820a49813620a4484f32bdf126685a40958
drishti-gupta0704 <drishti.gupta070424@gmail.com>
2026-03-10 20:54:15 +0530

    changed code so that queue service only handles the queue

--- services/queueService.js
+++ services/queueService.js
@@ -1,53 +1,15 @@
 
 const Queue = require("../models/Queue");
-const Ticket = require("../models/Ticket");
 
-const joinQueueService = async (userId, queueId) => {
-
-  // check queue exists
-  const queue = await Queue.findById(queueId);
-
-  if (!queue) {
-    throw new Error("Queue not found");
-  }
-
-  // check queue size
-  const waitingCount = await Ticket.countDocuments({
-    queue: queueId,
-    status: "waiting"
-  });
-
-  if (waitingCount >= queue.maxSize) {
-    throw new Error("Queue is full");
-  }
-
-  // check if user already in this queue
-  const existingTicket = await Ticket.findOne({
-    user: userId,
-    queue: queueId,
-    status: { $in: ["waiting", "serving"] }
-  });
-
-  if (existingTicket) {
-    throw new Error("User already in this queue");
-  }
-
-  // generate token (atomic increment)
-  const updatedQueue = await Queue.findByIdAndUpdate(
-    queueId,
-    { $inc: { currentToken: 1 } },
-    { new: true }
-  );
-
-  const tokenNumber = updatedQueue.currentToken;
-
-  const ticket = await Ticket.create({
-    user: userId,
-    queue: queueId,
-    tokenNumber
-  });
-
-  return ticket;
+const createQueueService = async (data) => {
+  return await Queue.create(data);
 };
 
-module.exports = { joinQueueService };
+const getQueuesService = async () => {
+  return await Queue.find();
+};
+
+module.exports = {
+  createQueueService,
+  getQueuesService
+};
--- services/ticketService.js
+++ services/ticketService.js
@@ -2,6 +2,53 @@
 const Ticket = require("../models/Ticket");
 const Queue = require("../models/Queue");
 
+// JOIN QUEUE
+const joinQueueService = async (userId, queueId) => {
+
+  const queue = await Queue.findById(queueId);
+
+  if (!queue) {
+    throw new Error("Queue not found");
+  }
+
+  const waitingCount = await Ticket.countDocuments({
+    queue: queueId,
+    status: "waiting"
+  });
+
+  if (waitingCount >= queue.maxSize) {
+    throw new Error("Queue is full");
+  }
+
+  const existingTicket = await Ticket.findOne({
+    user: userId,
+    queue: queueId,
+    status: { $in: ["waiting", "serving"] }
+  });
+
+  if (existingTicket) {
+    throw new Error("User already in this queue");
+  }
+
+  const updatedQueue = await Queue.findByIdAndUpdate(
+    queueId,
+    { $inc: { currentToken: 1 } },
+    { new: true }
+  );
+
+  const tokenNumber = updatedQueue.currentToken;
+
+  const ticket = await Ticket.create({
+    user: userId,
+    queue: queueId,
+    tokenNumber
+  });
+
+  return ticket;
+};
+
+
+// GET QUEUE POSITION
 const getQueuePosition = async (queueId, userId) => {
 
   const ticket = await Ticket.findOne({
@@ -31,5 +78,6 @@ const getQueuePosition = async (queueId, userId) => {
 };
 
 module.exports = {
+  joinQueueService,
   getQueuePosition
 };
