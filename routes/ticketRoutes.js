a0cf7579f953bf49e3a3dd7e0f165bd500126b54
drishti-gupta0704 <drishti.gupta070424@gmail.com>
2026-03-10 19:35:09 +0530

    updating routes

--- routes/ticketRoutes.js
+++ routes/ticketRoutes.js
@@ -2,10 +2,18 @@
 const express = require("express");
 const router = express.Router();
 
-const protect = require("../middleware/authMiddleware");
+const {
+  joinQueue,
+  getPosition,
+  cancelTicket
+} = require("../controllers/ticketController");
 
-const { joinQueue } = require("../controllers/ticketController");
+const authMiddleware = require("../middleware/authMiddleware");
 
-router.post("/join", protect, joinQueue);
+router.post("/join", authMiddleware, joinQueue);
+
+router.get("/position/:queueId", authMiddleware, getPosition);
+
+router.delete("/cancel/:ticketId", authMiddleware, cancelTicket);
 
 module.exports = router;
