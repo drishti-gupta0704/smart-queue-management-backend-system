2d6279bb831cb04885204d2033bba7ebea7947a6
drishti-gupta0704 <drishti.gupta070424@gmail.com>
2026-03-12 19:00:23 +0530

    adding validation errors

--- validation/authValidation.js
+++ validation/authValidation.js
@@ -1,5 +1,5 @@
 
-const { body } = require("express-validator");
+const { body, validationResult } = require("express-validator");
 
 const registerValidation = [
   body("name")
@@ -12,8 +12,17 @@ const registerValidation = [
     .withMessage("Valid email is required"),
   body("password")
     .isLength({ min: 6 })
-    .withMessage("Password must be at least 6 characters")
+    .withMessage("Password must be at least 6 characters"),
+  // middleware to handle validation result
+  (req, res, next) => {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      return res.status(400).json({ errors: errors.array() });
+    }
+    next();
+  }
 ];
+
 
 const loginValidation = [
   body("email")
@@ -21,7 +30,14 @@ const loginValidation = [
     .withMessage("Valid email is required"),
   body("password")
     .notEmpty()
-    .withMessage("Password is required")
+    .withMessage("Password is required"),
+  (req, res, next) => {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      return res.status(400).json({ errors: errors.array() });
+    }
+    next();
+  }
 ];
 
 module.exports = {
