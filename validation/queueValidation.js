4b2ae369d21d8967a6d1e35bd08dec959be5c604
drishti-gupta0704 <drishti.gupta070424@gmail.com>
2026-03-12 19:05:52 +0530

    validating creating queue

--- validation/queueValidation.js
+++ validation/queueValidation.js
@@ -1,17 +1,20 @@
 
-const { body } = require("express-validator");
+const { body, validationResult } = require("express-validator");
 
 const createQueueValidation = [
-  body("name")
-    .notEmpty()
-    .withMessage("Queue name is required"),
-  body("location")
-    .notEmpty()
-    .withMessage("Location is required"),
+  body("name").notEmpty().withMessage("Queue name is required"),
+  body("location").notEmpty().withMessage("Location is required"),
   body("maxSize")
     .optional()
     .isInt({ min: 1 })
-    .withMessage("maxSize must be a positive integer")
+    .withMessage("Max size must be a positive integer"),
+  (req, res, next) => {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      return res.status(400).json({ errors: errors.array() });
+    }
+    next();
+  }
 ];
 
 module.exports = {
