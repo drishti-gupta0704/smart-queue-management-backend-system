d1ce1c352cf59c79a9f1b0928af90024453ef609
drishti-gupta0704 <drishti.gupta070424@gmail.com>
2026-03-17 14:08:39 +0530

    implementing fetch ticket function

---
+++ public/js/tickets.js
@@ -0,0 +1,30 @@
+
+const token = localStorage.getItem("token");
+const API_URL = "http://localhost:4000/api";
+
+document.getElementById("backBtn").addEventListener("click", () => {
+  window.location.href = "dashboard.html";
+});
+
+
+async function fetchTickets() {
+  const res = await fetch(`${API_URL}/tickets/my-tickets`, {
+    headers: { Authorization: `Bearer ${token}` }
+  });
+  const data = await res.json();
+  const container = document.getElementById("tickets");
+  container.innerHTML = "";
+
+  data.tickets.forEach(t => {
+    const div = document.createElement("div");
+    const cancelBtn = document.createElement("button");
+    cancelBtn.textContent = "Cancel Ticket";
+    cancelBtn.addEventListener("click", () => cancelTicket(t._id));
+
+    div.innerHTML = `<h3>${t.queue.name}</h3>
+                     <p>Token: ${t.tokenNumber} | Status: ${t.status}</p>`;
+    div.appendChild(cancelBtn);
+    div.appendChild(document.createElement("hr"));
+    container.appendChild(div);
+  });
+}
